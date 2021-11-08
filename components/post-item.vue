f3161629bdc5ab3075a9ed8b26b5e77ea1575a5d
dumptyd <dumptyd2.0@gmail.com>
2021-11-08 09:16:57 +0530

    create post-item component for listing

---
+++ components/post-item.vue
@@ -0,0 +1,35 @@
+<template>
+<div class="post-item">
+  <time class="post-item__created-at">{{ createdAtFormattedShort }}</time>
+  <div>
+    <nuxt-link :to="href" class="post-item__link">{{ title }}</nuxt-link>
+    <div v-if="description" class="post-item__description">{{ description }}</div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+    createdAtFormattedShort: { type: String, required: true },
+    href: { type: String, required: true },
+    title: { type: String, required: true },
+    description: { type: String, default: '' }
+  }
+};
+</script>
+
+<style lang="scss">
+.post-item {
+  @apply mb-3 md:flex items-baseline;
+}
+.post-item__created-at {
+  @apply text-sm opacity-50 dark:opacity-60 w-24 flex-shrink-0;
+}
+.post-item__link {
+  @apply leading-normal hover:underline;
+}
+.post-item__description {
+  @apply text-sm opacity-60 mt-1;
+}
+</style>
