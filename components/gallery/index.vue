1d7a705ecdb2c65bf4d8e81e2636266d972139f2
dumptyd <dumptyd2.0@gmail.com>
2021-11-08 09:24:43 +0530

    add gallery component

---
+++ components/gallery/index.vue
@@ -0,0 +1,12 @@
+<template>
+<div class="my-8">
+  <div class="gallery">
+    <slot></slot>
+  </div>
+</div>
+</template>
+<style lang="scss">
+.gallery {
+  @apply row g-3;
+}
+</style>
---
+++ components/gallery/item.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="gallery__item">
+  <a :href="src" target="_blank" class="gallery__image-container" title="Open full size image">
+    <img :src="src" :alt="alt" class="gallery__image" loading="lazy">
+  </a>
+  <div class="gallery__caption">
+    <slot></slot>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+    src: { type: String, required: true },
+    alt: { type: String, default: '' }
+  }
+};
+</script>
+<style lang="scss">
+.gallery__item {
+  @apply col-6 md:col-4 lg:col-3;
+}
+.gallery__item .gallery__image-container {
+  @apply block w-full relative m-0 hover:no-underline;
+  padding-bottom: 100%;
+}
+.gallery__item .gallery__image {
+  @apply w-full h-full absolute top-0 left-0 object-cover m-0;
+}
+.gallery__caption {
+  @apply text-sm text-center;
+}
+</style>
