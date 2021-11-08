53f6aab412effa081bca11401925cc9562d98a0b
dumptyd <dumptyd2.0@gmail.com>
2021-11-08 09:12:11 +0530

    add about page

---
+++ pages/about.vue
@@ -0,0 +1,34 @@
+<template>
+<section class="about-author prose dark:prose-light">
+  <nuxt-content v-if="about" :document="about"></nuxt-content>
+</section>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content }) {
+    const about = await $content('about').fetch().catch(() => {});
+
+    return {
+      about
+    };
+  },
+  head: {
+    title: 'About'
+  }
+};
+</script>
+
+<style lang="scss">
+.about-author {
+  @apply max-w-none;
+}
+.about-author .nuxt-content {
+  > h2:first-child,
+  > h3:first-child,
+  > h4:first-child,
+  > h5:first-child{
+    @apply mt-0;
+  }
+}
+</style>
