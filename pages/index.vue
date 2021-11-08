7b4d29329c3bbe3bacc88fde569705c2a311ae54
dumptyd <dumptyd2.0@gmail.com>
2021-11-08 09:18:42 +0530

    implement post list page

---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+<section v-if="posts.length">
+  <post-item
+    v-for="post in posts" :key="post.slug"
+    v-bind="{
+      createdAtFormattedShort: post.createdAtFormattedShort,
+      href: post.href,
+      title: post.title,
+      description: post.description
+    }">
+  </post-item>
+</section>
+<div v-else>There are no posts here yet.</div>
+</template>
+
+<script>
+import { getPosts } from '~/utils';
+
+export default {
+  async asyncData({ $content }) {
+    const posts = await getPosts($content);
+
+    return {
+      posts
+    };
+  },
+  head: {
+    title: 'Posts'
+  }
+};
+</script>
