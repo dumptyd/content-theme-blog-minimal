a971509b7dbdd8921a57444122189c17ad65529b
dumptyd <dumptyd@users.noreply.github.com>
2021-11-08 04:07:59 +0000

    deploy: 13a73c72e19ebcb90baf84d7cd9b3cec732401d9

---
+++ _nuxt/static/1636344477/categories/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/categories", (function(a){return {data:[{categories:[{label:"Demo",slug:"demo",href:"\u002Fcategories\u002Fdemo",postCount:2},{label:"Documentation",slug:"documentation",href:"\u002Fcategories\u002Fdocumentation",postCount:1}]}],fetch:{"0":{hasTags:a,hasCategories:a,title:"content-theme-blog-minimal",showThemeLink:a}},mutations:void 0}}(true)));
