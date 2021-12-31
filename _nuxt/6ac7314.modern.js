74319ba71fcee148ecc9a746bcee4c6e35cf6487
dumptyd <dumptyd@users.noreply.github.com>
2021-12-31 05:17:28 +0000

    deploy: f0db72451c7de55844104858c94b40ecbe348130

---
+++ _nuxt/6ac7314.modern.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{251:function(n,o,c){"use strict";var e=c(9),t=Object(e.a)("HomeIcon",[["path",{d:"M3 9l9-7 9 7v11a2 2 0 01-2 2H5a2 2 0 01-2-2z"}],["polyline",{points:"9 22 9 12 15 12 15 22"}]]);o.a=t}}]);
